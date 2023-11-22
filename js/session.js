db42cdcbe63ff8dead69dbe04e1bd3bbdfae0c4d
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-11-22 21:59:14 +0200

    To function

--- js/session.js
+++ js/session.js
@@ -1,78 +1,71 @@
-// var x;
+console.log(typeof (8 / 0))
 
-// inline comment
-// x = 100;
+console.log(typeof ("test" / 5))
 
+/* Void function */
 
-/*
-bla
-bla
-bla
-bla
-*/
+// function sayOK() {
+//     console.log("OK")
+// }
 
-/*
-var x = 100;
-console.log(x)
-console.log(typeof x)
-var username = "Mohamed"
-console.log("***************")
-console.log(username)
-console.log(typeof username)
-console.log("***************")
+// var sayOK = function () {
+//     console.log("OK")
+// }
 
-console.log(true)
-console.log(false)
-
-console.log(document.getElementById("output"))
-
-/*
-var firstName = prompt("Please enter your first name"),
-    lastName = prompt("Please enter your last name"),
-    fullName = firstName + " " + lastName
-
-console.log(fullName)
-*/
-///////////////////////////////////////
-/*
-var firstNum = +prompt("Please enter your first number"),
-    lastNumber = +prompt("Please enter your last number");
-
-console.log(typeof firstNum)
-console.log(firstNum + lastNumber)
+var x = 55,
+    y = 10;
 
 
-var user = prompt("Please enter your name"),
-    age = prompt("Please enter your age");
-
-console.log("Hello " + user + " your age in days is " + age * 360)
-*/
-
-var person = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25,
-    marital_status: false,
-    country: {
-        country_one: "Egypt",
-        country_two: "USA"
-    },
-    military_status: undefined
+/* Returning value function */
+function calculation() {
+    return x + y
 }
 
-console.log(person)
-
-//Bracket notation
-
-console.log(person["firstName"])
-console.log(person["age"])
-console.log(person["country"]["country_one"])
-
-// Dot notation
-console.log(person.firstName)
-console.log("Hello " + person.firstName + " your age is " + person.age)
-console.log(person.age)
-console.log(person.country.country_one)
+console.log(calculation())
+console.log(calculation() * 5)
 
 
+/* Function with parameters 
+function addition(a, b) {
+    return a + b
+}
 
+console.log(addition(5, 9))
+console.log(addition(95, 90))
+
+var numOne = +prompt("Please enter first number"),
+    numTwo = +prompt("Please enter second number");
+
+console.log(addition(numOne, numTwo))
+console.log(addition(x, y))
+
+*/
+
+
+var trainees = ["Mohamed", "Mahmoud", "Omar", "Ali", "Bakr"]
+
+console.log(trainees)
+console.log(trainees.length)
+console.log(trainees[1])
+console.log(trainees[trainees.length - 1])
+console.log(trainees.at(-1))
+
+
+trainees[5] = "Osman";
+console.log(trainees)
+
+var clientName = prompt("Please enter your name"),
+    clientAge = +prompt("Please enter your age"),
+    currentYear = 2023
+
+
+function getAge(x) {
+    return currentYear - x
+}
+
+function displayClientData(name, year) {
+    console.log("Hello " + name + " Your age is " + getAge(year) + " and your age in days is " + getAge(year) * 365)
+}
+
+
+displayClientData(clientName, clientAge)
