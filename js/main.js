cf6243a6f1fc550f044f8153d42a33a6628eca73
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2023-12-27 22:45:04 +0200

    tart of string methods

--- js/main.js
+++ js/main.js
@@ -1,47 +1,49 @@
+var myName = "Mohamed",
+    lastName = "Ahmed",
+    job = "frontend developer and frontend instructor";
+
+console.log(myName);
+/**************************** length ************************************
+console.log(myName.length);
+
+/**************************** charAt ************************************
+
+console.log(myName.charAt(0))
+/**************************** charCodeAt ***************************
+
+console.log(lastName.charCodeAt(0))
+
+/**************************** fromCharCode ************************************
+
+console.log(String.fromCharCode(65))
+/**************************** concat ************************************
+
+console.log(myName.concat(lastName))
+
+/**************************** startsWith ************************************
+
+console.log(job.startsWith("front"))
+/**************************** endsWith ************************************
+
+console.log(job.endsWith("instructor"))
+
+/**************************** includes ************************************/
+
+console.log(job.includes("andd"))
 
 
-// not hoisted
-
-console.log(test)
-
-var test = function () {
-    console.log("Hello from test")
+/*
+for (let counter = 65; counter <= 90; counter++) {
+    console.log(String.fromCharCode(counter))
 }
-var test = function () {
-    console.log("Hello from test one again")
-}
-test()
 
-testTwo()
-
-function testTwo() {
-    console.log("Hello from test two")
+for (let counter = 97; counter <= 122; counter++) {
+    console.log(String.fromCharCode(counter))
 }
 
-function testTwo() {
-    console.log("Hello from test two again")
+
+for (let counter = 65; counter <= 122; counter++) {
+    if(counter > 90 && counter < 97) continue;
+    console.log(String.fromCharCode(counter))
 }
-
-console.log(5 + 5)
-console.log(5 + "5")
-
-console.log(5 * 5)
-console.log(5 * "5")
-console.log(5 * "5a")
-
-console.log(true + true)
-console.log(false + false)
-
-console.log(undefined + 5)
-console.log(undefined + "5")
-
-
-var arr = []
-if (arr) { console.log("Ok") }
-
-console.log(+"7878")
-console.log(Number("7878"))
-
-console.log(parseInt("5454"))
-console.log(parseInt("54.54"))
-console.log(parseFloat("88.12"))
+*/
